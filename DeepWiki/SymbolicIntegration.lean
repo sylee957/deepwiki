720b2ca401c6772f06487dfb5fb12511a58b4a2f
S.Y. Lee <sylee957@gmail.com>
2026-07-06 21:50:53 +0200

    Extract Groebner Buchberger criterion

--- DeepWiki/SymbolicIntegration.lean
+++ DeepWiki/SymbolicIntegration.lean
@@ -15,6 +15,7 @@ import DeepWiki.SymbolicIntegration.Core.Differential.PolynomialDerivatives
 import DeepWiki.SymbolicIntegration.Core.Polynomial.LinearFactors
 import DeepWiki.SymbolicIntegration.Core.Polynomial.GroebnerBasisBasic
 import DeepWiki.SymbolicIntegration.Core.Polynomial.GroebnerSPolynomial
+import DeepWiki.SymbolicIntegration.Core.Polynomial.GroebnerBuchbergerCriterion
 import DeepWiki.SymbolicIntegration.Core.Polynomial.LocalPrincipalAssembly
 import DeepWiki.SymbolicIntegration.Core.Polynomial.LocalPrincipalParts
 import DeepWiki.SymbolicIntegration.Core.Polynomial.RatFuncEvaluation
