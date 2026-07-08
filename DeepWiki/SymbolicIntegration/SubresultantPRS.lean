f5530b2e6cad7f784391c0858320b77c860725ea
S.Y. Lee <sylee957@gmail.com>
2026-07-08 22:26:55 +0200

    Make SubresultantPRS aggregator explicit

--- DeepWiki/SymbolicIntegration/SubresultantPRS.lean
+++ DeepWiki/SymbolicIntegration/SubresultantPRS.lean
@@ -1,6 +1,7 @@
+import DeepWiki.SymbolicIntegration.SubresultantPRS.Telescope
+import DeepWiki.SymbolicIntegration.SubresultantPRS.Remainder
 import DeepWiki.SymbolicIntegration.SubresultantPRS.ClosedForms
 
 /-! # Subresultant PRS
 
 Aggregates the subresultant PRS telescope, endpoint, and closed-form theorems. -/
-
